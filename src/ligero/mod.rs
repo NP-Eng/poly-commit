fdc1f717cd634e0d550c4c39307719a56288fc4b
mmagician <marcin.gorny.94@protonmail.com>
2023-09-25 17:24:11 +0200

    checking RHO_INV >=1 is too verbose of a sanity check

--- src/ligero/mod.rs
+++ src/ligero/mod.rs
@@ -172,10 +172,6 @@ where
         _num_vars: Option<usize>,
         _rng: &mut R,
     ) -> Result<Self::UniversalParams, Self::Error> {
-        assert!(
-            RHO_INV >= 1,
-            "RHO_INV is the inverse of the rate and must be at least 1"
-        );
         // The domain will have size m * RHO_INV, but we already have the first m elements
         GeneralEvaluationDomain::<F>::compute_size_of_domain(max_degree * (RHO_INV - 1))
             .ok_or(Error::UnsupportedDegreeBound(max_degree))?;
