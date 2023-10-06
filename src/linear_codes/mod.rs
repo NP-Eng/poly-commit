dc8bdc5ba4ea63a3a80b1f6be6925ae1cc0c658d
mmagician <marcin.gorny.94@protonmail.com>
2023-10-06 16:10:44 +0200

    expose Commitment struct

--- src/linear_codes/mod.rs
+++ src/linear_codes/mod.rs
@@ -29,7 +29,8 @@ mod data_structures;
 use data_structures::*;
 
 pub use data_structures::{
-    LinCodePCCommitterKey, LinCodePCProof, LinCodePCUniversalParams, LinCodePCVerifierKey, Metadata,
+    LinCodePCCommitment, LinCodePCCommitterKey, LinCodePCProof, LinCodePCUniversalParams,
+    LinCodePCVerifierKey, Metadata,
 };
 
 use utils::{calculate_t, get_indices_from_transcript, hash_column};
