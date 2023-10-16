45c93a476544dbb3ea3dbb5e69cf91cd6ac97c1d
Hossein Moghaddas <autquis@gmail.com>
2023-10-16 11:20:07 +0200

    Add a dummy test

--- src/linear_codes/univariate_breakdown/tests.rs
+++ src/linear_codes/univariate_breakdown/tests.rs
@@ -1,1 +1,11 @@
+#[cfg(test)]
+mod tests {
+    use crate::linear_codes::utils::calculate_t;
+    use ark_bls12_377::Fq;
 
+    #[test]
+    fn test_calculate_t_with_good_parameters() {
+        assert!(calculate_t::<Fq>(128, (4, 1), 2_usize.pow(32)).unwrap() < 200);
+        assert!(calculate_t::<Fq>(256, (4, 1), 2_usize.pow(32)).unwrap() < 400);
+    }
+}
