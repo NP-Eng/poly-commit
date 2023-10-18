705573b2aebb76de6b49860d13d8861f84a9c273
Antonio Mejías Gil <anmegi.95@gmail.com>
2023-10-18 16:36:04 +0200

    implemented size computation for ligero and hyrax commitments and proofs

--- src/linear_codes/data_structures.rs
+++ src/linear_codes/data_structures.rs
@@ -53,7 +53,7 @@ pub(crate) struct Metadata {
 pub struct LinCodePCCommitment<C: Config> {
     // number of rows resp. columns of the square matrix containing the coefficients of the polynomial
     pub(crate) metadata: Metadata,
-    pub(crate) root: C::InnerDigest,
+    pub root: C::InnerDigest,
 }
 
 impl<C: Config> PCCommitment for LinCodePCCommitment<C> {
@@ -96,18 +96,18 @@ impl PCRandomness for LinCodePCRandomness {
 /// Proof of an individual linear code well-formedness check or opening
 #[derive(Derivative, CanonicalSerialize, CanonicalDeserialize)]
 #[derivative(Default(bound = ""), Clone(bound = ""), Debug(bound = ""))]
-pub(crate) struct LinCodePCProofSingle<F, C>
+pub struct LinCodePCProofSingle<F, C>
 where
     F: PrimeField,
     C: Config,
 {
     /// For each of the indices in q, `paths` contains the path from the root of the merkle tree to the leaf
-    pub(crate) paths: Vec<Path<C>>,
+    pub paths: Vec<Path<C>>,
 
     /// v, s.t. E(v) = w
-    pub(crate) v: Vec<F>,
+    pub v: Vec<F>,
 
-    pub(crate) columns: Vec<Vec<F>>,
+    pub columns: Vec<Vec<F>>,
 }
 
 /// The Proof type for linear code PCS, which amounts to an array of individual proofs
@@ -118,8 +118,10 @@ where
     F: PrimeField,
     C: Config,
 {
-    pub(crate) opening: LinCodePCProofSingle<F, C>,
-    pub(crate) well_formedness: Option<Vec<F>>,
+    /// Proof of evaluation
+    pub opening: LinCodePCProofSingle<F, C>,
+    /// Proof of validity of commitment
+    pub well_formedness: Option<Vec<F>>,
 }
 
 // Multiple poly at one point
