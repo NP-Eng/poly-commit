17f7cafc86eeddffbc112decd4af87dd236820c6
mmagician <marcin.gorny.94@protonmail.com>
2023-10-19 17:00:39 +0200

    adapt benches to produce small coeffs

--- poly-commit/benches/hyrax_times.rs
+++ poly-commit/benches/hyrax_times.rs
@@ -1,8 +1,8 @@
 use ark_pcs_bench_templates::*;
-use ark_poly::{DenseMultilinearExtension, MultilinearExtension};
+use ark_poly::DenseMultilinearExtension;
 
 use ark_bn254::{Fr, G1Affine};
-use ark_ff::PrimeField;
+use ark_ff::{BigInteger, PrimeField};
 use ark_poly_commit::hyrax::HyraxPC;
 
 use rand_chacha::ChaCha20Rng;
@@ -14,7 +14,20 @@ fn rand_poly_hyrax<F: PrimeField>(
     num_vars: usize,
     rng: &mut ChaCha20Rng,
 ) -> DenseMultilinearExtension<F> {
-    DenseMultilinearExtension::rand(num_vars, rng)
+    let max_bits: usize = 60;
+    let num_bits = F::MODULUS_BIT_SIZE as usize;
+    let small_scalars = (0..(1 << num_vars))
+        .map(|_| {
+            let s = F::rand(rng).into_bigint();
+            let mut bits = s.to_bits_le();
+            bits.truncate(max_bits);
+            bits.resize(num_bits, false);
+            let bigint = F::BigInt::from_bits_le(&bits);
+            F::from_bigint(bigint).unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    DenseMultilinearExtension::from_evaluations_vec(num_vars, small_scalars)
 }
 
 fn rand_point_hyrax<F: PrimeField>(num_vars: usize, rng: &mut ChaCha20Rng) -> Vec<F> {
