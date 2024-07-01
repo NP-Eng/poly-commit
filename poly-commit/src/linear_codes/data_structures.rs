e4d29c5b30302ad617ba241583291ab77d7fd935
mmagician <marcin.gorny.94@protonmail.com>
2024-07-01 12:55:06 +0200

    make the root public

--- poly-commit/src/linear_codes/data_structures.rs
+++ poly-commit/src/linear_codes/data_structures.rs
@@ -95,9 +95,10 @@ pub(crate) struct Metadata {
 #[derive(Derivative, CanonicalSerialize, CanonicalDeserialize, Absorb)]
 #[derivative(Default(bound = ""), Clone(bound = ""), Debug(bound = ""))]
 pub struct LinCodePCCommitment<C: Config> {
-    // number of rows resp. columns of the square matrix containing the coefficients of the polynomial
+    /// number of rows resp. columns of the square matrix containing the coefficients of the polynomial
     pub(crate) metadata: Metadata,
-    pub(crate) root: C::InnerDigest,
+    /// root of the merkle tree
+    pub root: C::InnerDigest,
 }
 
 impl<C: Config> PCCommitment for LinCodePCCommitment<C> {
