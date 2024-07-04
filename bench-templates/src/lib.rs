94914371521f3bec623a71a444dd0c7197416400
Cesar199999 <cesar.descalzo2@gmail.com>
2024-07-04 12:29:28 +0200

    Use merlin sponge in benches

--- bench-templates/src/lib.rs
+++ bench-templates/src/lib.rs
@@ -1,6 +1,7 @@
 use ark_crypto_primitives::{
     crh::{sha256::digest::Digest, CRHScheme},
     sponge::{
+        merlin::Transcript,
         poseidon::{PoseidonConfig, PoseidonSponge},
         CryptographicSponge,
     },
@@ -244,30 +245,8 @@ where
 
 /*************** Auxiliary functions ***************/
 
-fn test_sponge<F: PrimeField>() -> PoseidonSponge<F> {
-    let full_rounds = 8;
-    let partial_rounds = 31;
-    let alpha = 17;
-
-    let mds = vec![
-        vec![F::one(), F::zero(), F::one()],
-        vec![F::one(), F::one(), F::zero()],
-        vec![F::zero(), F::one(), F::one()],
-    ];
-
-    let mut v = Vec::new();
-    let mut ark_rng = test_rng();
-
-    for _ in 0..(full_rounds + partial_rounds) {
-        let mut res = Vec::new();
-
-        for _ in 0..3 {
-            res.push(F::rand(&mut ark_rng));
-        }
-        v.push(res);
-    }
-    let config = PoseidonConfig::new(full_rounds, partial_rounds, alpha, mds, v, 2, 1);
-    PoseidonSponge::new(&config)
+fn test_sponge<F: PrimeField>() -> Transcript {
+    Transcript::new(b"test")
 }
 
 #[macro_export]
